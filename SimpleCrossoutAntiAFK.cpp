fe4db02174f79533a1bc8b905e03d981f52d6429
Purpursarkans <goga.danilov10@gmail.com>
2022-09-09 00:39:06 +0400

    .hpp

--- SimpleCrossoutAntiAFK.cpp
+++ SimpleCrossoutAntiAFK.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <windows.h>
-
-#define TAB 0x09
+#include "head.hpp"
 
 void PressButton(HWND hwnd, int key)
 {
---
+++ head.hpp
@@ -0,0 +1,8 @@
+#ifndef HEAD_HPP
+#define HEAD_HPP
+
+#include <iostream>
+#include <windows.h>
+#include "KEYS.HPP"
+
+#endif
