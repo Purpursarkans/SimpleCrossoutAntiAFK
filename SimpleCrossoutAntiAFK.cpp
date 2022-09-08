d83f1d29f09314b10019dde1ccb43923d9cdfac6
Purpursarkans <goga.danilov10@gmail.com>
2022-09-09 00:41:03 +0400

    keyW

--- SimpleCrossoutAntiAFK.cpp
+++ SimpleCrossoutAntiAFK.cpp
@@ -1,19 +1,22 @@
 #include "head.hpp"
 
-void PressButton(HWND hwnd, int key)
+void PressButton(HWND hwnd, int key, int PressOnMSec)
 {
     PostMessage(hwnd, WM_KEYDOWN, key, 0);
-    Sleep(34);
+    Sleep(PressOnMSec);
     PostMessage(hwnd, WM_KEYUP, key, 0);
     Sleep(50);
 }
 
 int main()
 {   
+
+    std::cout << "Set cursor on prog" << std::endl;
+    Sleep(3000);
     POINT P;
     GetCursorPos(&P);
     HWND hwnd = WindowFromPoint(P);
-    PressButton(hwnd, TAB);
+    PressButton(hwnd, KEY_W, 100);
     std::cout << hwnd << std::endl;
     system("pause");
 }
