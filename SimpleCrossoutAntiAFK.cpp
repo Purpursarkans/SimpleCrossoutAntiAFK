be815306c84896460025e7ece780e78c6064d191
Purpursarkans <goga.danilov10@gmail.com>
2022-09-09 00:36:41 +0400

    PressButton

--- SimpleCrossoutAntiAFK.cpp
+++ SimpleCrossoutAntiAFK.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <windows.h>
 
+#define TAB 0x09
+
+void PressButton(HWND hwnd, int key)
+{
+    PostMessage(hwnd, WM_KEYDOWN, key, 0);
+    Sleep(34);
+    PostMessage(hwnd, WM_KEYUP, key, 0);
+    Sleep(50);
+}
+
 int main()
 {   
     POINT P;
     GetCursorPos(&P);
     HWND hwnd = WindowFromPoint(P);
-    // PostMessage(hwnd, WM_KEYDOWN, VK_C, 0);
-    PostMessage(hwnd, WM_KEYDOWN, 8, 0);
+    PressButton(hwnd, TAB);
     std::cout << hwnd << std::endl;
     system("pause");
 }
