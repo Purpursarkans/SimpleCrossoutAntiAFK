6d247839df031989430d8fd352ea88236a1a5787
Purpursarkans <goga.danilov10@gmail.com>
2022-09-09 00:51:15 +0400

    infinity press

--- SimpleCrossoutAntiAFK.cpp
+++ SimpleCrossoutAntiAFK.cpp
@@ -2,30 +2,37 @@
 
 void PressButton(HWND hwnd, int key, int PressOnMSec)
 {
+    Beep(100,100);
     PostMessage(hwnd, WM_KEYDOWN, key, 0);
     Sleep(PressOnMSec);
     PostMessage(hwnd, WM_KEYUP, key, 0);
-    Sleep(50);
+    Sleep(1000);
 }
 
-void antiAfk(HWND hwnd, int key, int PressOnMSec, int sleep)
-{
-    for(;;)
-    {
-        PressButton(hwnd, key, PressOnMSec);
-        Sleep(sleep);
-    }
-}
 
 int main()
 {   
 
     std::cout << "Set cursor on prog" << std::endl;
-    Sleep(3000);
+    for(int i = 3; i > 0; i--)
+    {
+        std::cout << i << " sec sleep" << std::endl;
+        Sleep(1000);
+    }
+    
     POINT P;
     GetCursorPos(&P);
     HWND hwnd = WindowFromPoint(P);
-    antiAfk(hwnd, KEY_W, 300, 3000);
     std::cout << hwnd << std::endl;
+    for (;;)
+    {
+        PressButton(hwnd, KEY_W, 500);
+        PressButton(hwnd, KEY_SPACE, 500);
+        PressButton(hwnd, KEY_S, 500);
+        PressButton(hwnd, KEY_SPACE, 500);
+        Sleep(3000);
+    }
+
+    
     system("pause");
 }
