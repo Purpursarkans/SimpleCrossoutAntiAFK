5979aef42ff47a90e537572c86abee56924ead2c
Purpursarkans <goga.danilov10@gmail.com>
2022-09-09 00:44:13 +0400

    antiAFK

--- SimpleCrossoutAntiAFK.cpp
+++ SimpleCrossoutAntiAFK.cpp
@@ -8,6 +8,15 @@ void PressButton(HWND hwnd, int key, int PressOnMSec)
     Sleep(50);
 }
 
+void antiAfk(HWND hwnd, int key, int PressOnMSec, int sleep)
+{
+    for(;;)
+    {
+        PressButton(hwnd, key, PressOnMSec);
+        Sleep(sleep);
+    }
+}
+
 int main()
 {   
 
@@ -16,7 +25,7 @@ int main()
     POINT P;
     GetCursorPos(&P);
     HWND hwnd = WindowFromPoint(P);
-    PressButton(hwnd, KEY_W, 100);
+    antiAfk(hwnd, KEY_W, 300, 3000);
     std::cout << hwnd << std::endl;
     system("pause");
 }
