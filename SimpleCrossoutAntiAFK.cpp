8dca198e84a47da8fac43eba221e8be799d79a7e
Purpursarkans <goga.danilov10@gmail.com>
2022-09-09 00:29:12 +0400

    hwnd from point

--- SimpleCrossoutAntiAFK.cpp
+++ SimpleCrossoutAntiAFK.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <windows.h>
 
 int main()
-{
-    std::cout << "hi" << std::endl;
+{   
+    POINT P;
+    GetCursorPos(&P);
+    HWND hwnd = WindowFromPoint(P);
+    // PostMessage(hwnd, WM_KEYDOWN, VK_C, 0);
+    PostMessage(hwnd, WM_KEYDOWN, 8, 0);
+    std::cout << hwnd << std::endl;
     system("pause");
 }
